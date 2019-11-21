3604ece90c9ada1766cc7d778066d771fcdd82dc
ostry1414 <ostry1414@gmail.com>
2019-11-21 14:57:32 +0100

    20.11

--- src/main/java/staticClass/User.java
+++ src/main/java/staticClass/User.java
@@ -6,21 +6,35 @@ public class User {
     private String login;
     private String password;
 
+    public User(String x, String x1) {
+
+    }
+
     public static void incrementGlobalId(){
         globalId++;
     }
-
-    public User (String login, String password){
-        this.userId;
-        this.login;
-        this.password;
-        incrementGlobalId();
-
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                '}';
     }
 
-    public User(int userId, String login, String password) {
-        this.userId = userId;
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
         this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
     }
 }
